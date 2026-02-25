zhaocongqi/ovn

commit 52bb1dabffbc5dbb2a7450c2ba8fbd891b5b2ec6

northd: Copy ACL IDs to the southbound DB.
This introduces a new southbound table called ACL_ID for storing
persistent ACL conntrack IDs. These IDs are generated by ovn-northd for
any allow-related ACL that has the "persist-established" option set.

Signed-off-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Ales Musil <amusil@redhat.com>

northd/inc-proc-northd.c
@@ -43,6 +43,7 @@
 #include "en-sync-sb.h"
 #include "en-sync-from-sb.h"
 #include "en-ecmp-nexthop.h"
+#include "en-acl-ids.h"
 #include "unixctl.h"
 #include "util.h"
 
@@ -105,7 +106,8 @@ static unixctl_cb_func chassis_features_list;
     SB_NODE(static_mac_binding, "static_mac_binding") \
     SB_NODE(chassis_template_var, "chassis_template_var") \
     SB_NODE(logical_dp_group, "logical_dp_group") \
-    SB_NODE(ecmp_nexthop, "ecmp_nexthop")
+    SB_NODE(ecmp_nexthop, "ecmp_nexthop") \
+    SB_NODE(acl_id, "acl_id")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -166,6 +168,7 @@ static ENGINE_NODE(bfd, "bfd");
 static ENGINE_NODE(bfd_sync, "bfd_sync");
 static ENGINE_NODE(ecmp_nexthop, "ecmp_nexthop");
 static ENGINE_NODE(multicast_igmp, "multicast_igmp");
+static ENGINE_NODE(acl_id, "acl_id");
 
 void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                           struct ovsdb_idl_loop *sb)
@@ -190,6 +193,9 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_global_config, &en_sb_chassis,
                      global_config_sb_chassis_handler);
     engine_add_input(&en_global_config, &en_sampling_app, NULL);
+
+    engine_add_input(&en_acl_id, &en_nb_acl, NULL);
+    engine_add_input(&en_acl_id, &en_sb_acl_id, NULL);
 
     engine_add_input(&en_northd, &en_nb_mirror, NULL);
     engine_add_input(&en_northd, &en_nb_static_mac_binding, NULL);
@@ -364,6 +370,8 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                      northd_output_fdb_aging_handler);
     engine_add_input(&en_northd_output, &en_ecmp_nexthop,
                      northd_output_ecmp_nexthop_handler);
+    engine_add_input(&en_northd_output, &en_acl_id,
+                     northd_output_acl_id_handler);
 
     struct engine_arg engine_arg = {
         .nb_idl = nb->idl,

northd/automake.mk
@@ -38,6 +38,8 @@ northd_ovn_northd_SOURCES = \
 	northd/en-ls-stateful.h \
 	northd/en-sampling-app.c \
 	northd/en-sampling-app.h \
+	northd/en-acl-ids.c \
+	northd/en-acl-ids.h \
 	northd/inc-proc-northd.c \
 	northd/inc-proc-northd.h \
 	northd/ipam.c \

northd/en-acl-ids.c
@@ -1,0 +1,100 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "en-acl-ids.h"
+#include "lib/uuidset.h"
+#include "lib/ovn-sb-idl.h"
+#include "lib/ovn-nb-idl.h"
+#include "lib/bitmap.h"
+
+#include "openvswitch/vlog.h"
+
+VLOG_DEFINE_THIS_MODULE(northd_acl_ids);
+
+#define MAX_ACL_ID 65535
+
+void *
+en_acl_id_init(struct engine_node *node OVS_UNUSED,
+               struct engine_arg *arg OVS_UNUSED)
+{
+    return NULL;
+}
+
+static bool
+should_sync_to_sb(const struct nbrec_acl *nb_acl)
+{
+    return !strcmp(nb_acl->action, "allow-related") &&
+           smap_get_bool(&nb_acl->options,
+                         "persist-established",
+                         false);
+}
+
+void
+en_acl_id_run(struct engine_node *node, void *data OVS_UNUSED)
+{
+    const struct engine_context *eng_ctx = engine_get_context();
+    const struct nbrec_acl_table *nb_acl_table =
+        EN_OVSDB_GET(engine_get_input("NB_acl", node));
+    const struct sbrec_acl_id_table *sb_acl_id_table =
+        EN_OVSDB_GET(engine_get_input("SB_acl_id", node));
+    struct uuidset visited = UUIDSET_INITIALIZER(&visited);
+    unsigned long *id_bitmap = bitmap_allocate(MAX_ACL_ID);
+
+    const struct nbrec_acl *nb_acl;
+    const struct sbrec_acl_id *sb_id;
+    SBREC_ACL_ID_TABLE_FOR_EACH_SAFE (sb_id, sb_acl_id_table) {
+        nb_acl = nbrec_acl_table_get_for_uuid(nb_acl_table,
+                                              &sb_id->header_.uuid);
+        if (nb_acl && should_sync_to_sb(nb_acl)) {
+            bitmap_set1(id_bitmap, sb_id->id);
+            uuidset_insert(&visited, &sb_id->header_.uuid);
+        } else {
+            sbrec_acl_id_delete(sb_id);
+        }
+    }
+
+    size_t scan_start = 1;
+    size_t scan_end = MAX_ACL_ID;
+    NBREC_ACL_TABLE_FOR_EACH (nb_acl, nb_acl_table) {
+        if (uuidset_find_and_delete(&visited, &nb_acl->header_.uuid)) {
+            continue;
+        }
+        if (!should_sync_to_sb(nb_acl)) {
+            continue;
+        }
+        int64_t new_id = bitmap_scan(id_bitmap, 0,
+                                     scan_start, scan_end + 1);
+        if (new_id == scan_end + 1) {
+            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
+            VLOG_WARN_RL(&rl, "Exhausted all ACL IDs");
+            break;
+        }
+        sb_id = sbrec_acl_id_insert_persist_uuid(eng_ctx->ovnsb_idl_txn,
+                                                 &nb_acl->header_.uuid);
+        sbrec_acl_id_set_id(sb_id, new_id);
+        bitmap_set1(id_bitmap, new_id);
+        scan_start = new_id + 1;
+    }
+
+    engine_set_node_state(node, EN_UPDATED);
+    uuidset_destroy(&visited);
+    bitmap_free(id_bitmap);
+}
+
+void
+en_acl_id_cleanup(void *data OVS_UNUSED)
+{
+}

northd/en-acl-ids.h
@@ -1,0 +1,13 @@
+#ifndef EN_ACL_IDS_H
+#define EN_ACL_IDS_H
+
+#include <config.h>
+#include <stdbool.h>
+
+#include "lib/inc-proc-eng.h"
+
+bool northd_acl_id_handler(struct engine_node *node, void *data);
+void *en_acl_id_init(struct engine_node *, struct engine_arg *);
+void en_acl_id_run(struct engine_node *, void *data);
+void en_acl_id_cleanup(void *data);
+#endif

northd/en-northd-output.c
@@ -80,3 +80,11 @@ northd_output_ecmp_nexthop_handler(struct engine_node *node,
     engine_set_node_state(node, EN_UPDATED);
     return true;
 }
+
+bool
+northd_output_acl_id_handler(struct engine_node *node,
+                             void *data OVS_UNUSED)
+{
+    engine_set_node_state(node, EN_UPDATED);
+    return true;
+}

northd/en-northd-output.h
@@ -19,5 +19,7 @@ bool northd_output_fdb_aging_handler(struct engine_node *node,
                                      void *data OVS_UNUSED);
 bool northd_output_ecmp_nexthop_handler(struct engine_node *node,
                                         void *data OVS_UNUSED);
+bool northd_output_acl_id_handler(struct engine_node *node,
+                                  void *data OVS_UNUSED);
 
 #endif

northd/northd.h
@@ -63,6 +63,9 @@ struct northd_input {
     struct eth_addr svc_monitor_mac_ea;
     const struct chassis_features *features;
 
+    /* ACL ID inputs. */
+    const struct acl_id_data *acl_id_data;
+
     /* Indexes */
     struct ovsdb_idl_index *sbrec_chassis_by_name;
     struct ovsdb_idl_index *sbrec_chassis_by_hostname;

northd/ovn-northd.c
@@ -945,6 +945,10 @@ main(int argc, char *argv[])
         ovsdb_idl_omit_alert(ovnsb_idl_loop.idl,
                              &sbrec_ecmp_nexthop_columns[i]);
     }
+    for (size_t i = 0; i < SBREC_ACL_ID_N_COLUMNS; i++) {
+        ovsdb_idl_omit_alert(ovnsb_idl_loop.idl,
+                             &sbrec_acl_id_columns[i]);
+    }
 
     unixctl_command_register("sb-connection-status", "", 0, 0,
                              ovn_conn_show, ovnsb_idl_loop.idl);

commit 37a2ba36558069255676163ce2573276615724fe

northd: Remove redundant engine node input.
The northd_output node added the acl_id node as input twice. First it
was added as an input with no change handler. Then later it was added as
an input with a change handler.

This commit fixes the issue by adding acl_id only once, with the change
handler.

The only real effect this had was making for a bit of a confusing graph
when visualizing the northd incremental engine.

Fixes: 52bb1dabffbc ("northd: Copy ACL IDs to the southbound DB.")
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

northd/inc-proc-northd.c
@@ -480,7 +480,8 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_sync_from_sb, &en_sb_service_monitor, NULL);
     engine_add_input(&en_sync_from_sb, &en_sb_ha_chassis_group, NULL);
 
-    engine_add_input(&en_northd_output, &en_acl_id, NULL);
+    engine_add_input(&en_northd_output, &en_acl_id,
+                     northd_output_acl_id_handler);
     engine_add_input(&en_northd_output, &en_sync_from_sb, NULL);
     engine_add_input(&en_northd_output, &en_sync_to_sb,
                      northd_output_sync_to_sb_handler);
@@ -492,8 +493,6 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                      northd_output_fdb_aging_handler);
     engine_add_input(&en_northd_output, &en_ecmp_nexthop,
                      northd_output_ecmp_nexthop_handler);
-    engine_add_input(&en_northd_output, &en_acl_id,
-                     northd_output_acl_id_handler);
     engine_add_input(&en_northd_output, &en_advertised_route_sync,
                      northd_output_advertised_route_sync_handler);
     engine_add_input(&en_northd_output, &en_advertised_mac_binding_sync,

tests/ovn-inc-proc-graph-dump.at
@@ -235,14 +235,13 @@ digraph "Incremental-Processing-Engine" {
 	SB_advertised_mac_binding -> advertised_mac_binding_sync [[label=""]];
 	northd -> advertised_mac_binding_sync [[label="engine_noop_handler"]];
 	northd_output [[style=filled, shape=box, fillcolor=white, label="northd_output"]];
-	acl_id -> northd_output [[label=""]];
+	acl_id -> northd_output [[label="northd_output_acl_id_handler"]];
 	sync_from_sb -> northd_output [[label=""]];
 	sync_to_sb -> northd_output [[label="northd_output_sync_to_sb_handler"]];
 	lflow -> northd_output [[label="northd_output_lflow_handler"]];
 	mac_binding_aging -> northd_output [[label="northd_output_mac_binding_aging_handler"]];
 	fdb_aging -> northd_output [[label="northd_output_fdb_aging_handler"]];
 	ecmp_nexthop -> northd_output [[label="northd_output_ecmp_nexthop_handler"]];
-	acl_id -> northd_output [[label="northd_output_acl_id_handler"]];
 	advertised_route_sync -> northd_output [[label="northd_output_advertised_route_sync_handler"]];
 	advertised_mac_binding_sync -> northd_output [[label="northd_output_advertised_mac_binding_sync_handler"]];
 }